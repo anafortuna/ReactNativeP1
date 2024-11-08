99d6b6299c0051f02508d9c3787a741bf30a341d
anafortuna <alobofortuna@gmail.com>
2024-11-08 16:43:59 -0300

    Header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 import { styles } from './styles'
+import Logo from './src/assets/logo.png'
+import Stroke from './src/assets/stroke.png'
+import Message from './src/assets/message.png'
 export default function App() {
 
   return <View style={styles.container}>
-    
+    <View style={styles.header}>
+      <Image source={Logo} />
+      <View style={styles.headerOptions}>
+        <Image source={Stroke} />
+        <Image source={Message} />
+      </View>
+    </View>
   </View >
 }
 
--- styles.ts
+++ styles.ts
@@ -6,6 +6,19 @@ export const styles = StyleSheet.create({
         backgroundColor: '#000',
         alignItems: 'center',
     },
- 
-
+    header: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 56,
+        width: '100%',
+        flexDirection: 'row',
+        paddingHorizontal: 10,
+        marginTop: 40,
+    },
+    headerOptions: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        gap: 10,
+        paddingRight: 5,
+    },
 })
